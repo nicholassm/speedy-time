006f83695499f1dbdd9873b6efd035de89337703
Nicholas Schultz-Møller <nicholassm@gmail.com>
2015-12-26 18:58:10 +0100

    Use  computations for data fields.

--- src/com/nicholassm/lmf/time/DateTime.java
+++ src/com/nicholassm/lmf/time/DateTime.java
@@ -9,42 +9,80 @@ public class DateTime {
 	private static final long MILLIS_IN_A_MINUTE = MILLIS_IN_A_SECOND * 60L;
 	private static final long MILLIS_IN_AN_HOUR  = MILLIS_IN_A_MINUTE * 60L;
 	private static final long MILLIS_IN_A_DAY    = MILLIS_IN_AN_HOUR * 24L;
-	private static final long MILLIS_IN_A_MONTH  = MILLIS_IN_A_DAY * 30L;
-	private static final long MILLIS_IN_A_YEAR   = MILLIS_IN_A_DAY * 365L;
 
 	// TODO: Only work with positive numbers? (For speed).
 	public DateTime(long utcMillis) {
 		this.utcMillis = utcMillis;
 	}
 
-	// TODO: Idea: Precalculate 1. Jan. 00:00:00.000 for each year and store in an array. Index is
-	// years after 1970. Do a binary search to find year.
-	// For months and days: Subtract start of year in millis.
-	// Now two cases: (1) Non-leap year and (2) leap year.
+	public static DateTime now() {
+		return new DateTime(System.currentTimeMillis());
+	}
 
-	// TODO: Take leap years into account:
 	public static int yearOf(long utcMillis) {
-		long epochDay = utcMillis / MILLIS_IN_A_DAY;
-		// TODO: Continue here: How to calculate the year.
-		return (int) (epochDay + DAYS_0000_TO_1970);
+		assert utcMillis >= 0;
+
+		long epochDay   = utcMillis / MILLIS_IN_A_DAY;
+		long zeroDay    = epochDay + DAYS_0000_TO_1970;
+		// Find the March-based year.
+		zeroDay -= 60; // Adjust to 0000-03-01 so leap day is at end of four year cycle.
+		long yearEst    = (400 * zeroDay + 591) / DAYS_PER_CYCLE;
+		long doyEst     = zeroDay - (365 * yearEst + yearEst / 4 - yearEst / 100 + yearEst / 400);
+		int marchDoy0   = (int) doyEst;
+		// Convert march-based values back to January-based.
+		int marchMonth0 = (marchDoy0 * 5 + 2) / 153;
+		yearEst += marchMonth0 / 10;
+
+		return (int) yearEst;
 	}
 
-	// TODO: Take leap years into account:
 	public static int monthOf(long utcMillis) {
-		return (int) ((utcMillis % MILLIS_IN_A_YEAR) / MILLIS_IN_A_MONTH);
+		assert utcMillis >= 0;
+
+		long epochDay   = utcMillis / MILLIS_IN_A_DAY;
+		long zeroDay    = epochDay + DAYS_0000_TO_1970;
+		// Find the March-based year.
+		zeroDay -= 60; // Adjust to 0000-03-01 so leap day is at end of four year cycle.
+		long yearEst    = (400 * zeroDay + 591) / DAYS_PER_CYCLE;
+		long doyEst     = zeroDay - (365 * yearEst + yearEst / 4 - yearEst / 100 + yearEst / 400);
+		int marchDoy0   = (int) doyEst;
+		// Convert march-based values back to January-based.
+		int marchMonth0 = (marchDoy0 * 5 + 2) / 153;
+		int month       = (marchMonth0 + 2) % 12 + 1;
+
+		return month;
 	}
 
-	// TODO: Take leap years into account:
-	public static int dayOf(long utcMillis) {
-		return (int) ((utcMillis % MILLIS_IN_A_MONTH) / MILLIS_IN_A_DAY);
+	public static int dayOfMonthOf(long utcMillis) {
+		assert utcMillis >= 0;
+
+		long epochDay   = utcMillis / MILLIS_IN_A_DAY;
+		long zeroDay    = epochDay + DAYS_0000_TO_1970;
+		// Find the March-based year.
+		zeroDay -= 60; // Adjust to 0000-03-01 so leap day is at end of four year cycle.
+		long yearEst    = (400 * zeroDay + 591) / DAYS_PER_CYCLE;
+		long doyEst     = zeroDay - (365 * yearEst + yearEst / 4 - yearEst / 100 + yearEst / 400);
+		int marchDoy0   = (int) doyEst;
+		// Convert march-based values back to January-based.
+		int marchMonth0 = (marchDoy0 * 5 + 2) / 153;
+		int dom         = marchDoy0 - (marchMonth0 * 306 + 5) / 10 + 1;
+
+		return dom;
 	}
 
 	public static int hourOf(long utcMillis) {
+		assert utcMillis >= 0;
 		return (int) ((utcMillis % MILLIS_IN_A_DAY) / MILLIS_IN_AN_HOUR);
 	}
 
 	public static int minuteOf(long utcMillis) {
+		assert utcMillis >= 0;
 		return (int) ((utcMillis % MILLIS_IN_AN_HOUR) / MILLIS_IN_A_MINUTE);
+	}
+
+	public static int secondsOf(long utcMillis) {
+		assert utcMillis >= 0;
+		return (int) ((utcMillis % MILLIS_IN_A_MINUTE) / MILLIS_IN_A_SECOND);
 	}
 
 	public long asUtcMillis() {
@@ -52,10 +90,8 @@ public class DateTime {
 	}
 
 	public static void main(String[] args) {
-		System.out.println(yearOf(System.currentTimeMillis()));
-		System.out.println(monthOf(System.currentTimeMillis()));
-		System.out.println(dayOf(System.currentTimeMillis()));
-		System.out.println(hourOf(System.currentTimeMillis()));
-		System.out.println(minuteOf(System.currentTimeMillis()));
+		long now = System.currentTimeMillis();
+		System.out.println(yearOf(now)+"-"+monthOf(now)+"-"+dayOfMonthOf(now));
+		System.out.println(hourOf(now)+":"+minuteOf(now)+":"+secondsOf(now));
 	}
 }
